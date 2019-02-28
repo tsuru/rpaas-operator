b52a43baf5f528203fcab01ede567e7d24c93c0f
Guilherme Garnier <guilherme.garnier@gmail.com>
2019-02-28 10:53:59 -0300

    Update pkg/apis code for operator-sdk 0.5.0

--- pkg/apis/extensions/v1alpha1/rpaasinstance_types.go
+++ pkg/apis/extensions/v1alpha1/rpaasinstance_types.go
@@ -8,6 +8,7 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // RpaasInstanceSpec defines the desired state of RpaasInstance
+// +k8s:openapi-gen=true
 type RpaasInstanceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
@@ -26,12 +27,16 @@ type RpaasInstanceSpec struct {
 	// Locations are configuration file fragments used as location blocks in
 	// nginx config.
 	Locations []Location `json:"locations,omitempty"`
+
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
 // RpaasInstanceStatus defines the observed state of RpaasInstance
+// +k8s:openapi-gen=true
 type RpaasInstanceStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- pkg/apis/extensions/v1alpha1/rpaasplan_types.go
+++ pkg/apis/extensions/v1alpha1/rpaasplan_types.go
@@ -8,17 +8,22 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // RpaasPlanSpec defines the desired state of RpaasPlan
+// +k8s:openapi-gen=true
 type RpaasPlanSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	Image  string      `json:"image"`
 	Config NginxConfig `json:"config"`
+
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
 // RpaasPlanStatus defines the observed state of RpaasPlan
+// +k8s:openapi-gen=true
 type RpaasPlanStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
