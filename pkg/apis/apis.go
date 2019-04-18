3fcb013812201d2f4a324df09fdf90358589401b
Claudio Netto <nettinhorama@gmail.com>
2019-04-18 19:39:01 -0300

    fix field selector query for CustomResources
    This change adds field indexing for RpaasInstance and RpaasPlan names to allow
    querying from field selector.

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,7 +1,9 @@
 package apis
 
 import (
+	"github.com/tsuru/rpaas-operator/pkg/apis/extensions/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // AddToSchemes may be used to add all resources defined in the project to a Scheme
@@ -10,4 +12,26 @@ var AddToSchemes runtime.SchemeBuilder
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
 	return AddToSchemes.AddToScheme(s)
+}
+
+// AddFieldIndexes adds some indexes on fieldIndexer so their objects can
+// later be fetched by a field selector.
+func AddFieldIndexes(fieldIndexer client.FieldIndexer) (err error) {
+	err = fieldIndexer.IndexField(
+		&v1alpha1.RpaasInstance{},
+		"metadata.name",
+		func(o runtime.Object) []string { return []string{o.(*v1alpha1.RpaasInstance).Name} },
+	)
+
+	if err != nil {
+		return
+	}
+
+	err = fieldIndexer.IndexField(
+		&v1alpha1.RpaasPlan{},
+		"metadata.name",
+		func(o runtime.Object) []string { return []string{o.(*v1alpha1.RpaasPlan).Name} },
+	)
+
+	return
 }
