b41deb2b0460aa37025b15f14fc2996317719c13
Claudio Netto <nettinhorama@gmail.com>
2019-05-02 13:43:22 -0300

    rpaas: group the functions to handling configuration blocks

--- rpaas/manager.go
+++ rpaas/manager.go
@@ -6,6 +6,26 @@ import (
 	"github.com/tsuru/rpaas-operator/pkg/apis/extensions/v1alpha1"
 )
 
+// ConfigurationBlockHandler defines some functions to handle the custom
+// configuration blocks from an instance.
+type ConfigurationBlockHandler interface {
+	// DeleteBlock removes the configuration block named by blockName. It returns
+	// a nil error meaning it was successful, otherwise a non-nil one which
+	// describes the reached problem.
+	DeleteBlock(instanceName, blockName string) error
+
+	// ListBlocks returns all custom configuration blocks from instance (which
+	// name is instanceName). It returns a nil error meaning it was successful,
+	// otherwise a non-nil one which describes the reached problem.
+	ListBlocks(instanceName string) (map[string]string, error)
+
+	// UpdateBlock overwrites the older configuration block content with the one.
+	// Whether the configuration block entry does not exist, it will already be
+	// created with the new content. It returns a nil error meaning it was
+	// successful, otherwise a non-nil one which describes the reached problem.
+	UpdateBlock(instanceName, blockName, content string) error
+}
+
 type CreateArgs struct {
 	Name        string   `json:"name" form:"name"`
 	Plan        string   `json:"plan" form:"plan"`
@@ -19,11 +39,10 @@ type CreateArgs struct {
 }
 
 type RpaasManager interface {
+	ConfigurationBlockHandler
+
 	UpdateCertificate(instance string, cert tls.Certificate) error
 	CreateInstance(args CreateArgs) error
 	DeleteInstance(name string) error
 	GetInstance(name string) (*v1alpha1.RpaasInstance, error)
-	DeleteBlock(instanceName, block string) error
-	ListBlocks(instanceName string) (map[string]string, error)
-	UpdateBlock(instanceName, block, content string) error
 }
