fc6a28e1e92fa3bd5392c9f670a544009869fdf6
Cezar Sa Espinola <cezarsa@gmail.com>
2019-05-08 18:59:25 -0300

    api,rpaas: allow multiple named certificates

--- rpaas/fake/manager.go
+++ rpaas/fake/manager.go
@@ -8,7 +8,7 @@ import (
 )
 
 type RpaasManager struct {
-	FakeUpdateCertificate func(string, tls.Certificate) error
+	FakeUpdateCertificate func(instance, name string, cert tls.Certificate) error
 	FakeCreateInstance    func(args rpaas.CreateArgs) error
 	FakeDeleteInstance    func(name string) error
 	FakeGetInstance       func(name string) (*v1alpha1.RpaasInstance, error)
@@ -17,9 +17,9 @@ type RpaasManager struct {
 	FakeUpdateBlock       func(instanceName string, block rpaas.ConfigurationBlock) error
 }
 
-func (m *RpaasManager) UpdateCertificate(instance string, c tls.Certificate) error {
+func (m *RpaasManager) UpdateCertificate(instance, name string, c tls.Certificate) error {
 	if m.FakeUpdateCertificate != nil {
-		return m.FakeUpdateCertificate(instance, c)
+		return m.FakeUpdateCertificate(instance, name, c)
 	}
 	return nil
 }
