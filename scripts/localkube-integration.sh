82044d1759a427e9e273337cbbf08a2f9c6a1c51
Claudio Netto <nettinhorama@gmail.com>
2019-05-22 19:25:54 -0300

    ci: run integration tests w/ kind instead of minikube

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -1,23 +1,173 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-# Download kubectl, which is a requirement for using minikube.
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-# Download minikube.
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.35.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --feature-gates=CustomResourceSubresources=true --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
-# Fix the kubectl context, as it's often stale.
-sudo minikube update-context
-sudo chown -R $(id -un):$(id -gn) /home/travis/.minikube
+download_kubectl() {
+  local version="${1:-"v1.14.1"}"
+  local destination="${2:-".tmp"}"
 
-# Wait for Kubernetes to be up and ready.
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do echo 'node not ready yet'; kubectl describe nodes; sleep 10; done
+  local kubectl_bin="${destination}/kubectl"
 
-make build
-kubectl create namespace rpaas-operator-integration
-sed -ie 's/imagePullPolicy: Always/imagePullPolicy: Never/g' deploy/operator.yaml
-ls deploy/crds/*_crd.yaml | xargs -I{} kubectl apply -f {}
-kubectl apply -f deploy/ --namespace rpaas-operator-integration
+  [[ -f "${kubectl_bin}" ]] && echo -n "${kubectl_bin}" && return
+
+  local os="$(get_os)"
+  local arch="$(get_arch)"
+
+  curl -sLo ${kubectl_bin} "https://storage.googleapis.com/kubernetes-release/release/${version}/bin/${os}/${arch}/kubectl" && \
+  chmod +x ${kubectl_bin}
+  echo -n ${kubectl_bin}
+}
+
+download_kind() {
+  local version="${1:-"v0.3.0"}"
+  local destination="${2:-".tmp"}"
+
+  local kind_bin="${destination}/kind"
+
+  [[ -f "${kind_bin}" ]] && echo -n "${kind_bin}" && return
+
+  local os="$(get_os)"
+  local arch="$(get_arch)"
+
+  curl -sLo "${kind_bin}" "https://github.com/kubernetes-sigs/kind/releases/download/${version}/kind-${os}-${arch}" && \
+  chmod +x "${kind_bin}"
+  echo -n "${kind_bin}"
+}
+
+get_os() {
+  local os="$(uname -s)"
+  case ${os} in
+    Linux)
+      echo -n "linux";;
+    Darwin)
+      echo -n "darwin";;
+    *)
+      echo "Unsupported operating system. (supported: Linux or Darwin)"
+      exit 1;;
+  esac
+}
+
+get_arch() {
+  local arch="$(uname -m)"
+  case ${arch} in
+    amd64|x86_64)
+      echo -n "amd64";;
+    *)
+      echo "Unsupported machine architecture. (supported: x86_64 and amd64)"
+      exit 1;;
+  esac
+}
+
+create_k8s_cluster() {
+  local kind_bin="${1}"
+  local name="${2}"
+  local wait="${3:-"10m"}"
+
+  ${kind_bin} create cluster \
+    --name "${cluster_name}" --wait "10m" \
+    --image "kindest/node:${KUBERNETES_VERSION:-"v1.14.1"}"
+}
+
+delete_k8s_cluster() {
+  local kind_bin="${1}"
+  local name=${2}
+
+  ${kind_bin} delete cluster --name "${name}"
+}
+
+run_nginx_operator() {
+  local kubectl_bin="${1}"
+  local namespace="${2}"
+
+  local nginx_operator_dir="vendor/github.com/tsuru/nginx-operator"
+
+  ${kubectl_bin} -n ${namespace} apply -f ${nginx_operator_dir}/deploy
+
+  sed -E "s/(namespace:) (.+)/\1 ${namespace}/" ${nginx_operator_dir}/deploy/role_binding.yaml |
+  ${kubectl_bin} -n ${namespace} apply -f -
+
+  ls ${nginx_operator_dir}/deploy/crds/*_crd.yaml |
+  xargs -I{} ${kubectl_bin} -n ${namespace} apply -f {}
+}
+
+run_rpaas_operator() {
+  local kubectl_bin="${1}"
+  local namespace="${2}"
+  local kind_bin="${3}"
+  local cluster_name="${4}"
+  local tag="${5:-"integration"}"
+
+  echo "Building container images of RPaaS operator and API using tag \"${tag}\"..."
+  make build TAG="${tag}"
+
+  echo tsuru/rpaas-{api,operator} | tr ' ' '\n' |
+  xargs -I{} ${kind_bin} load docker-image --name ${cluster_name} {}
+
+  ls ./deploy | grep -E ".+\.yaml" | grep -vE "api|operator" |
+  xargs -I{} ${kubectl_bin} -n ${namespace} apply -f ./deploy/{}
+
+  sed -E "s/(namespace:) (.+)/\1 ${namespace}/" ./deploy/role_binding.yaml |
+  ${kubectl_bin} -n ${namespace} apply -f -
+
+  sed -e 's/imagePullPolicy: Always/imagePullPolicy: Never/' deploy/operator.yaml |
+  sed -e "s|tsuru/rpaas-operator|&:${tag}|" |
+  ${kubectl_bin} -n ${namespace} apply -f -
+
+  sed -e 's/imagePullPolicy: Always/imagePullPolicy: Never/' deploy/api.yaml |
+  sed -e "s|tsuru/rpaas-api|&:${tag}|" |
+  ${kubectl_bin} -n ${namespace} apply -f -
+
+  ls ./deploy/crds/*_crd.yaml |
+  xargs -I{} ${kubectl_bin} -n ${namespace} apply -f {}
+}
+
+[[ -n ${DEBUG} ]] && set -x
+
+# When KUBERNETES_VERSION isn't defined, use v1.14.1
+[[ -z ${KUBERNETES_VERSION} ]] && export KUBERNETES_VERSION="v1.14.1"
+
+local_tmp_dir="$(pwd)/.tmp"
+mkdir -p "${local_tmp_dir}"
+echo "Using temporary dir: ${local_tmp_dir}"
+echo
+
+kind_version="v0.3.0"
+echo "Downloading the kind (Kubernetes-IN-Docker)..." && \
+kind_bin="$(download_kind ${kind_version} ${local_tmp_dir})" && \
+echo "kind path: ${kind_bin} " && \
+echo "kind version: ${kind_version}" && \
+echo
+
+cluster_name="rpaasv2-integration"
+echo "Creating a Kubernetes cluster \"${cluster_name}\"..." && \
+create_k8s_cluster "${kind_bin}" "${cluster_name}" && \
+echo "Kubernetes version: ${KUBERNETES_VERSION}"
+echo
+
+echo "Downloading the kubectl..." && \
+kubectl_bin="$(download_kubectl)" && \
+kubeconfig="$(${kind_bin} get kubeconfig-path --name ${cluster_name})" && \
+echo "kubectl path: ${kubectl_bin}" && \
+echo "kubectl version: ${KUBERNETES_VERSION}" && \
+echo "kubeconfig path: ${kubeconfig}" && \
+
+export KUBECONFIG="${kubeconfig}"
+
+# show some info about Kubernetes cluster
+${kubectl_bin} cluster-info && \
+${kubectl_bin} get all
+
+rpaas_system_namespace="rpaas-system"
+
+echo "Using namespace \"${rpaas_system_namespace}\" to run \"nginx-operator\" and \"rpaas-operator\"..."
+${kubectl_bin} create namespace "${rpaas_system_namespace}"
+
+run_nginx_operator "${kubectl_bin}" "${rpaas_system_namespace}" "${local_tmp_dir}"
+run_rpaas_operator "${kubectl_bin}" "${rpaas_system_namespace}" "${kind_bin}" "${cluster_name}"
+
 sleep 30s
-kubectl get deployment --all-namespaces
-kubectl get pods --all-namespaces
+
+${kubectl_bin} get deployment --all-namespaces
+${kubectl_bin} get pods --all-namespaces
+
 RPAAS_OPERATOR_INTEGRATION=1 go test ./...
+
+delete_k8s_cluster "${kind_bin}" "${cluster_name}"
