110369e7d880d9edf7020e9e7340bf4954b92bc7
Claudio Netto <nettinhorama@gmail.com>
2019-05-29 20:08:58 -0300

    integration: include local tmp dir into PATH before running the tests

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -173,7 +173,12 @@ local_rpaas_api_port=39999
 ${kubectl_bin} -n "${rpaas_system_namespace}" port-forward svc/rpaas-api ${local_rpaas_api_port}:9999 --address=127.0.0.1 &
 kubectl_port_forward_pid=${!}
 
-RPAAS_API_ADDRESS="http://127.0.0.1:${local_rpaas_api_port}" RPAAS_OPERATOR_INTEGRATION=1 go test ./...
+sleep 10s
+
+PATH="${PATH}:$(pwd)/.tmp"                                   \
+RPAAS_API_ADDRESS="http://127.0.0.1:${local_rpaas_api_port}" \
+RPAAS_OPERATOR_INTEGRATION=1                                 \
+go test ./...
 
 kill ${kubectl_port_forward_pid}
 
