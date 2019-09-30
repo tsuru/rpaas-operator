400b900252a4826bc52c4f28af418605d5c6cb38
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2019-09-30 14:05:18 -0300

    fix: refactoring proxy_test.go

--- cmd/plugin/cli/proxy/proxy_test.go
+++ cmd/plugin/cli/proxy/proxy_test.go
@@ -10,28 +10,28 @@ import (
 	"gotest.tools/assert"
 )
 
-type mockServer struct {
+type MockServer struct {
 	ts            *httptest.Server
 	getURLfunc    func(string) (string, error)
 	getTargetFunc func() (string, error)
 	readTokenFunc func() (string, error)
 }
 
-func (ms *mockServer) GetURL(path string) (string, error) {
+func (ms *MockServer) GetURL(path string) (string, error) {
 	if ms.getURLfunc == nil {
 		return ms.ts.URL, nil
 	}
 	return ms.getURLfunc(path)
 }
 
-func (ms *mockServer) GetTarget() (string, error) {
+func (ms *MockServer) GetTarget() (string, error) {
 	if ms.getTargetFunc == nil {
 		return "", nil
 	}
 	return ms.getTargetFunc()
 }
 
-func (ms *mockServer) ReadToken() (string, error) {
+func (ms *MockServer) ReadToken() (string, error) {
 	if ms.readTokenFunc == nil {
 		return "", nil
 	}
@@ -51,15 +51,40 @@ func TestProxy(t *testing.T) {
 			prox: Proxy{ServiceName: "",
 				InstanceName: "",
 				Method:       "GET",
-				Server: &mockServer{getTargetFunc: func() (string, error) {
+				Server: &MockServer{getTargetFunc: func() (string, error) {
 					return "", errors.New("Error while aquiring target")
 				}},
 			},
 			assertion: func(t *testing.T, err error) {
-				assert.Error(t, err, err.Error())
 				assert.Equal(t, err.Error(), "Error while aquiring target")
 			},
 			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			},
+		},
+		{
+			name: "testing all valid",
+			prox: Proxy{ServiceName: "",
+				InstanceName: "",
+				Method:       "GET",
+				Server:       &MockServer{},
+			},
+			assertion: func(t *testing.T, err error) {
+				assert.NilError(t, err)
+			},
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				helper := []struct {
+					instanceName string `json:"name"`
+					serviceName  string `json:"service"`
+				}{
+					{
+						instanceName: "rpaas-instance-test",
+						serviceName:  "rpaas-service-test",
+					},
+				}
+				body, _ := json.Marshal(helper)
+				w.Write(body)
 				w.WriteHeader(http.StatusOK)
 			},
 		},
@@ -69,28 +94,12 @@ func TestProxy(t *testing.T) {
 				ServiceName:  "rpaas-service-test",
 				InstanceName: "rpaas-instance-test",
 				Method:       "GET",
-				Server: &mockServer{getTargetFunc: func() (string, error) {
+				Server: &MockServer{getURLfunc: func(string) (string, error) {
 					return "", errors.New("Error while parsing URL")
 				}},
 			},
 			assertion: func(t *testing.T, err error) {
-				assert.Error(t, err, err.Error())
 				assert.Equal(t, err.Error(), "Error while parsing URL")
-			},
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Content-Type", "application/json")
-				helper := []struct {
-					instanceName string `json:"name"`
-					serviceName  string `json:"service"`
-				}{
-					{
-						instanceName: "rpaas-instance-test",
-						serviceName:  "rpaas-service-test",
-					},
-				}
-				body, _ := json.Marshal(helper)
-				w.Write(body)
-				w.WriteHeader(http.StatusOK)
 			},
 		},
 		{
@@ -99,34 +108,19 @@ func TestProxy(t *testing.T) {
 				ServiceName:  "rpaas-service-test",
 				InstanceName: "rpaas-instance-test",
 				Method:       "GET",
-				Server: &mockServer{getTargetFunc: func() (string, error) {
+				Server: &MockServer{readTokenFunc: func() (string, error) {
 					return "", errors.New("Error while parsing token")
 				}},
 			},
 			assertion: func(t *testing.T, err error) {
-				assert.Error(t, err, err.Error())
 				assert.Equal(t, err.Error(), "Error while parsing token")
-			},
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Content-Type", "application/json")
-				helper := []struct {
-					instanceName string `json:"name"`
-					serviceName  string `json:"service"`
-				}{
-					{
-						instanceName: "rpaas-instance-test",
-						serviceName:  "rpaas-service-test",
-					},
-				}
-				body, _ := json.Marshal(helper)
-				w.Write(body)
-				w.WriteHeader(http.StatusOK)
 			},
 		},
 	}
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.testServer = httptest.NewServer(tt.handler)
+			tt.prox.Server.(*MockServer).ts = tt.testServer
 			_, err := tt.prox.ProxyRequest()
 			tt.assertion(t, err)
 		})
