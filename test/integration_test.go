aac1c08832fe11ce67b4d4283d596d0e325d7235
Claudio Netto <nettinhorama@gmail.com>
2021-07-22 20:23:13 -0300

    test(integration): covering cert-manager support with integration tests

--- test/integration_test.go
+++ test/integration_test.go
@@ -99,9 +99,26 @@ func Test_RpaasOperator(t *testing.T) {
 					KeyField:         "default.key",
 					KeyPath:          "my-custom-name.key",
 				},
+				{
+					CertificateField: "cert-manager.crt",
+					KeyField:         "cert-manager.key",
+				},
 			},
 		}
 		assert.Equal(t, tlsSecret, nginx.Spec.Certificates)
+
+		certificatesSecret := &corev1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "v1",
+				Kind:       "Secret",
+			},
+		}
+		err = get(certificatesSecret, nginx.Spec.Certificates.SecretName, namespaceName)
+		require.NoError(t, err)
+		assert.NotEmpty(t, certificatesSecret.Data["default.crt"])
+		assert.NotEmpty(t, certificatesSecret.Data["default.key"])
+		assert.NotEmpty(t, certificatesSecret.Data["cert-manager.crt"])
+		assert.NotEmpty(t, certificatesSecret.Data["cert-manager.key"])
 
 		nginxService := &corev1.Service{
 			TypeMeta: metav1.TypeMeta{
