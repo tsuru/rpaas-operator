18e588f3072f25845a0564bdf28869a6ff04c736
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-05-12 17:06:50 -0300

    add fake server factory

---
+++ internal/web/target/fake-server.go
@@ -0,0 +1,37 @@
+package target
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/tsuru/rpaas-operator/internal/pkg/rpaas"
+	extensionsruntime "github.com/tsuru/rpaas-operator/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var _ Factory = &fakeServerFactory{}
+
+type fakeServerFactory struct {
+	manager rpaas.RpaasManager
+}
+
+func (f *fakeServerFactory) Manager(ctx context.Context, header http.Header) (rpaas.RpaasManager, error) {
+	return f.manager, nil
+}
+
+func NewFakeServerFactory() (Factory, error) {
+
+	scheme := extensionsruntime.NewScheme()
+	k8sClient := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects().Build()
+
+	manager, err := rpaas.NewK8S(nil, k8sClient, "", "")
+	if err != nil {
+		return nil, err
+	}
+
+	return &fakeServerFactory{manager: manager}, nil
+}
+
+func NewFakeFactory(manager rpaas.RpaasManager) Factory {
+	return &fakeServerFactory{manager: manager}
+}
