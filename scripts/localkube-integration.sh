57d477e79428c3f30eb4f43d22688c5046b97c48
Claudio Netto <nettinhorama@gmail.com>
2022-07-16 15:29:09 -0300

    fix(integration): change path to new local bin directory

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -117,7 +117,7 @@ main() {
 
   make build/plugin/rpaasv2
 
-  RPAAS_PLUGIN_BIN=$(pwd)/build/_output/bin/rpaasv2            \
+  RPAAS_PLUGIN_BIN=$(pwd)/bin/rpaasv2                          \
   RPAAS_API_ADDRESS="http://127.0.0.1:${local_rpaas_api_port}" \
   RPAAS_OPERATOR_INTEGRATION=1                                 \
   go test -v ./test/...
