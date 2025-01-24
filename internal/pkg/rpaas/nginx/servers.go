d2f776de4346109a61e848b3fbb79a2061646feb
Wilson Júnior <wilsonpjunior@gmail.com>
2025-01-24 15:17:21 -0300

    Implement server management logic and add tests for ProduceServers function

---
+++ internal/pkg/rpaas/nginx/servers.go
@@ -0,0 +1,144 @@
+package nginx
+
+import (
+	"sort"
+	"strings"
+
+	nginxv1alpha1 "github.com/tsuru/nginx-operator/api/v1alpha1"
+	"github.com/tsuru/rpaas-operator/api/v1alpha1"
+)
+
+type Server struct {
+	Name          string `json:"names"`
+	TLS           bool   `json:"tls"`
+	TLSSecretName string `json:"secretName"`
+	Default       bool   `json:"default,omitempty"`
+	Wildcard      bool   `json:"wildcard,omitempty"`
+
+	Locations []v1alpha1.Location `json:"locations,omitempty"`
+}
+
+func ProduceServers(spec *v1alpha1.RpaasInstanceSpec) []*Server {
+	defaultServer := &Server{
+		Default: true,
+	}
+
+	mapServerNames := make(map[string]*Server)
+	defaultLocationsIndex := map[string]int{}
+
+	for _, server := range spec.Locations {
+		if server.ServerName != "" {
+			mapServerNames[server.ServerName] = &Server{
+				Name: server.ServerName,
+			}
+		}
+	}
+
+	for _, location := range spec.Locations {
+		if location.ServerName != "" {
+			continue
+		}
+		defaultServer.Locations = append(defaultServer.Locations, *location.DeepCopy())
+		defaultLocationsIndex[location.Path] = len(defaultLocationsIndex)
+
+		for _, server := range mapServerNames {
+			server.Locations = append(server.Locations, location)
+		}
+
+	}
+
+	for _, location := range spec.Locations {
+		if location.ServerName == "" {
+			continue
+		}
+
+		if index, ok := defaultLocationsIndex[location.Path]; ok {
+			mapServerNames[location.ServerName].Locations[index] = *location.DeepCopy()
+		} else {
+			mapServerNames[location.ServerName].Locations = append(mapServerNames[location.ServerName].Locations, *location.DeepCopy())
+		}
+
+	}
+
+	wildCardTLS := map[string]nginxv1alpha1.NginxTLS{}
+
+	for _, tls := range spec.TLS {
+		for _, host := range tls.Hosts {
+			if isWildCard(host) {
+				wildCardTLS[host] = tls
+			}
+		}
+	}
+
+	var appendTLSServer = func(host string, tlsSecretName string) {
+		if _, ok := mapServerNames[host]; !ok {
+			mapServerNames[host] = &Server{
+				Name:      host,
+				Locations: deepCopyLocations(defaultServer.Locations),
+			}
+		}
+
+		server := mapServerNames[host]
+		server.TLS = true
+		server.TLSSecretName = tlsSecretName
+		server.Wildcard = isWildCard(host)
+
+	}
+
+	for _, tls := range spec.TLS {
+		for _, host := range tls.Hosts {
+			appendTLSServer(host, tls.SecretName)
+		}
+	}
+
+	for host, server := range mapServerNames {
+		if server.TLS {
+			continue
+		}
+
+		// find possible wildcard
+		possibleWildcard := "*." + strings.SplitN(host, ".", 2)[1]
+		if tls, ok := wildCardTLS[possibleWildcard]; ok {
+			appendTLSServer(host, tls.SecretName)
+		}
+	}
+
+	exactServers := []*Server{}
+	wildcardServers := []*Server{}
+
+	for _, server := range mapServerNames {
+		if server.Wildcard {
+			wildcardServers = append(wildcardServers, server)
+		} else {
+			exactServers = append(exactServers, server)
+		}
+	}
+
+	sortServers(exactServers)
+	sortServers(wildcardServers)
+
+	result := []*Server{defaultServer}
+	result = append(result, exactServers...)
+	result = append(result, wildcardServers...)
+
+	return result
+}
+
+func sortServers(servers []*Server) {
+	sort.Slice(servers, func(i, j int) bool {
+		return servers[i].Name < servers[j].Name
+	})
+}
+
+func deepCopyLocations(locations []v1alpha1.Location) []v1alpha1.Location {
+	if len(locations) == 0 {
+		return nil
+	}
+	copiedLocations := make([]v1alpha1.Location, len(locations))
+	copy(copiedLocations, locations)
+	return copiedLocations
+}
+
+func isWildCard(host string) bool {
+	return strings.HasPrefix(host, "*.")
+}
---
+++ internal/pkg/rpaas/nginx/servers_test.go
@@ -0,0 +1,404 @@
+package nginx
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	nginxv1alpha1 "github.com/tsuru/nginx-operator/api/v1alpha1"
+	"github.com/tsuru/rpaas-operator/api/v1alpha1"
+)
+
+func TestProduceServers(t *testing.T) {
+	tests := []struct {
+		name     string
+		spec     *v1alpha1.RpaasInstanceSpec
+		expected []*Server
+	}{
+
+		{
+			name: "spec with locations without server name",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				Locations: []v1alpha1.Location{
+					{
+						Path:        "/",
+						Destination: "http://example.com",
+					},
+					{
+						Path:        "/test",
+						Destination: "http://test.example.com",
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/",
+							Destination: "http://example.com",
+						},
+						{
+							Path:        "/test",
+							Destination: "http://test.example.com",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "spec with locations with many server names",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				Locations: []v1alpha1.Location{
+					{
+						Path:        "/common",
+						Destination: "http://common.com",
+					},
+					{
+						Path:        "/",
+						ServerName:  "example.com",
+						Destination: "http://example.com",
+					},
+					{
+						Path:        "/",
+						ServerName:  "test.example.com",
+						Destination: "http://test.example.com",
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+					},
+				},
+				{
+					Name: "example.com",
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+						{
+							Path:        "/",
+							ServerName:  "example.com",
+							Destination: "http://example.com",
+						},
+					},
+				},
+				{
+					Name: "test.example.com",
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+						{
+							Path:        "/",
+							ServerName:  "test.example.com",
+							Destination: "http://test.example.com",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "spec with locations with many server names with override",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				Locations: []v1alpha1.Location{
+					{
+						Path:        "/",
+						Destination: "http://common.com",
+					},
+					{
+						Path:        "/",
+						ServerName:  "example.com",
+						Destination: "http://example.com",
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/",
+							Destination: "http://common.com",
+						},
+					},
+				},
+				{
+					Name: "example.com",
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/",
+							ServerName:  "example.com",
+							Destination: "http://example.com",
+						},
+					},
+				},
+			},
+		},
+
+		{
+			name: "spec with tls",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				TLS: []nginxv1alpha1.NginxTLS{
+					{
+						SecretName: "example.org",
+						Hosts:      []string{"example.org"},
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+				},
+				{
+					TLS:           true,
+					TLSSecretName: "example.org",
+					Name:          "example.org",
+				},
+			},
+		},
+
+		{
+			name: "spec with tls and locations",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				TLS: []nginxv1alpha1.NginxTLS{
+					{
+						SecretName: "example.org",
+						Hosts:      []string{"example.org"},
+					},
+				},
+				Locations: []v1alpha1.Location{
+					{
+						Path: "/",
+						Content: &v1alpha1.Value{
+							Value: "# My custom NGINX config",
+						},
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+				{
+					TLS:           true,
+					TLSSecretName: "example.org",
+					Name:          "example.org",
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			name: "spec with tls with wildcard",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				TLS: []nginxv1alpha1.NginxTLS{
+					{
+						SecretName: "wild-card-example.org",
+						Hosts:      []string{"*.example.org"},
+					},
+				},
+				Locations: []v1alpha1.Location{
+					{
+						Path: "/",
+						Content: &v1alpha1.Value{
+							Value: "# My custom NGINX config",
+						},
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+				{
+					TLS:           true,
+					TLSSecretName: "wild-card-example.org",
+					Name:          "*.example.org",
+					Wildcard:      true,
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "spec with tls with wildcard ad specific blocks",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				TLS: []nginxv1alpha1.NginxTLS{
+					{
+						SecretName: "wild-card-example.org",
+						Hosts:      []string{"*.example.org"},
+					},
+				},
+				Locations: []v1alpha1.Location{
+					{
+						Path: "/",
+						Content: &v1alpha1.Value{
+							Value: "# My custom NGINX config",
+						},
+					},
+
+					{
+						Path:       "/info",
+						ServerName: "info.example.org",
+						Content: &v1alpha1.Value{
+							Value: "# My custom NGINX config",
+						},
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+				{
+					TLS:           true,
+					TLSSecretName: "wild-card-example.org",
+					Name:          "info.example.org",
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+
+						{
+							Path:       "/info",
+							ServerName: "info.example.org",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+				{
+					Name:          "*.example.org",
+					TLS:           true,
+					TLSSecretName: "wild-card-example.org",
+					Wildcard:      true,
+					Locations: []v1alpha1.Location{
+						{
+							Path: "/",
+							Content: &v1alpha1.Value{
+								Value: "# My custom NGINX config",
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			name: "spec with locations with many server names",
+			spec: &v1alpha1.RpaasInstanceSpec{
+				Locations: []v1alpha1.Location{
+					{
+						Path:        "/common",
+						Destination: "http://common.com",
+					},
+					{
+						Path:        "/",
+						ServerName:  "example.com",
+						Destination: "http://example.com",
+					},
+					{
+						Path:        "/",
+						ServerName:  "test.example.com",
+						Destination: "http://test.example.com",
+					},
+				},
+			},
+			expected: []*Server{
+				{
+					Default: true,
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+					},
+				},
+				{
+					Name: "example.com",
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+						{
+							Path:        "/",
+							ServerName:  "example.com",
+							Destination: "http://example.com",
+						},
+					},
+				},
+				{
+					Name: "test.example.com",
+					Locations: []v1alpha1.Location{
+						{
+							Path:        "/common",
+							Destination: "http://common.com",
+						},
+						{
+							Path:        "/",
+							ServerName:  "test.example.com",
+							Destination: "http://test.example.com",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := ProduceServers(tt.spec)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
