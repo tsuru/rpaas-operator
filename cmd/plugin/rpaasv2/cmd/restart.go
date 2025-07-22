c7c1b2753e5f417822f18e8e8ab242cf1f4efe33
ravilock <ravi.me.professional@hotmail.com>
2025-07-22 16:34:14 -0300

    feat(plugin): add restart command

---
+++ cmd/plugin/rpaasv2/cmd/restart.go
@@ -0,0 +1,48 @@
+// Copyright 2025 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+func NewCmdRestart() *cli.Command {
+	return &cli.Command{
+		Name:  "restart",
+		Usage: "Restarts instance if the current state is shutdown",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "service",
+				Aliases: []string{"tsuru-service", "s"},
+				Usage:   "the Tsuru service name",
+			},
+			&cli.StringFlag{
+				Name:     "instance",
+				Aliases:  []string{"tsuru-service-instance", "i"},
+				Usage:    "the reverse proxy instance name",
+				Required: true,
+			},
+		},
+		Before: setupClient,
+		Action: runRestart,
+	}
+}
+
+func runRestart(c *cli.Context) error {
+	client, err := getClient(c)
+	if err != nil {
+		return err
+	}
+
+	err = client.Restart(c.Context, c.String("instance"))
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(c.App.Writer, "Restarting instance %s\n", formatInstanceName(c))
+	return nil
+}
---
+++ cmd/plugin/rpaasv2/cmd/restart_test.go
@@ -0,0 +1,34 @@
+// Copyright 2024 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tsuru/rpaas-operator/pkg/rpaas/client/fake"
+)
+
+func TestRestart(t *testing.T) {
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+
+	args := []string{"./rpaasv2", "restart", "-s", "some-service", "-i", "my-instance"}
+
+	client := &fake.FakeClient{
+		FakeRestart: func(instance string) error {
+			require.Equal(t, instance, "my-instance")
+			return nil
+		},
+	}
+
+	app := NewApp(stdout, stderr, client)
+	err := app.Run(args)
+	require.NoError(t, err)
+	assert.Equal(t, stdout.String(), "Restarting instance some-service/my-instance\n")
+}
